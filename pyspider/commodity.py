8fe90dbaa1bf4586d8e59d9319d3d66bd2c0c81a
freemanmengyuan <1647786154@qq.com>
2018-09-24 01:07:45 +0800

    mengyuan-pyspider-获取分类信息

--- pyspider/commodity.py
+++ pyspider/commodity.py
@@ -3,7 +3,13 @@
 # Created on 2018-08-16 02:18:24
 # Project: yao-site
 
+'''
+获取分类信息
+基础的抓取信息
+'''
+
 from pyspider.libs.base_handler import *
+import json
 
 
 class Handler(BaseHandler):
@@ -14,22 +20,43 @@ class Handler(BaseHandler):
         self.url = 'https://www.yaofang.cn/c/autokey/categoryAll'
 
     @every(minutes=24 * 60)
-    def on_start(self):
-        print(self.url)
-        self.crawl(self.url, callback=self.index_page)
+    def on_start(self):  # 脚本入口
+        print(self.url)  # 打印连接
+        self.crawl(self.url, callback=self.index_page)  # 添加任务至调度器
 
     @config(age=10 * 24 * 60 * 60)
     def index_page(self, response):
-        for each in response.doc('.hd').items():
-            print(each('a').attr.href)
-            self.crawl(each('a').attr.href, callback=self.detail_page)
+        cate = list()
+        one_num = 1
+        two_num = 20
+        three_num = 100
+        # 获取顶级分类
+        for menu in response.doc('.yMenuList').items():
+            one_cate = menu('.fenleiTit').html()
+            one_cate_true = one_cate[50:-1:1] #切片
+            #print(one_cate_true)
+            dect = {'id':one_num, 'name':one_cate_true, 'parent_id':0, 'catch_url': ''}
+            cate.append(dect)
+            # 获取二级分类
+            for two_menu in menu('dl').items():
+                #print(min_menu)
+                two_cate_name = two_menu('dt a').text()
+                two_cate_url = two_menu('dt a').attr('href')
+                dect = {'id': two_num, 'name': two_cate_name, 'parent_id': one_num, 'catch_url': two_cate_url}
+                cate.append(dect)
+                #获取三级分类
+                for min_menu in menu('dl dd a').items():
+                    #print(min_menu)
+                    min_cate_name = min_menu.text()
+                    min_cate_url = min_menu.attr('href')
+                    dect = {'id': three_num, 'name': min_cate_name, 'parent_id': two_num, 'catch_url': min_cate_url}
+                    cate.append(dect)
+                    three_num = three_num + 1
+                two_num = two_num + 1
+            one_num = one_num + 1
 
-    @config(priority=2)
-    def detail_page(self, response):
-        return {
-            "url": response.url,
-            "title": response.doc('title').text(),
-            'sort': response.doc('.rating_num').text(),
-        }
+        #print(json.dumps(cate))
+
+
 
 
