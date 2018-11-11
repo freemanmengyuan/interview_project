5731ebdb76e6049b677981c05ec9adea621c067b
freemanmengyuan <1647786154@qq.com>
2018-11-11 23:42:16 +0800

    爬取商品信息-done

--- pyspider/yf_goods.py
+++ pyspider/yf_goods.py
@@ -7,13 +7,14 @@
 获取商品信息
 嵌套抓取
 考虑入库效率先写入文件
-后续多线程入库
+后续协程入库
 '''
 
 from pyspider.libs.base_handler import *
 import json
 #import MySQLdb
 import time
+import re
 
 class Handler(BaseHandler):
     crawl_config = {
@@ -21,7 +22,7 @@ class Handler(BaseHandler):
 
     def __init__(self):
         self.s_tasks = self.read_log('/root/workspace/python/category_2018-10-21.log')
-
+        self.goods = list()
 
     @every(minutes=24 * 60)
     def on_start(self):  # 脚本入口
@@ -29,16 +30,59 @@ class Handler(BaseHandler):
         #print(list_tasks[0:10:1])
         for task in list_tasks:
             if task['type'] == 3:
-                self.crawl(task['catch_url'], callback=self.index_page, save={'cate_id':task['id'], 'type':task['type']})
+                self.crawl(
+                    task['catch_url'], callback=self.switch_page,
+                    save={'cate_id':task['id'], 'type':task['type']}
+                )
+        print(self.goods)
         #self.crawl(self.url, callback=self.index_page)  # 添加任务至调度器
 
+    # 判断是否需要请求分页
     #@config(age=10 * 24 * 60 * 60)
-    def index_page(self, response):
+    def switch_page(self, response):
         #获取分类id 标示药品分类
         cate_id  = response.save['cate_id']
         str_page_count = response.doc('.fp_total').html()
-        list_page = list(str_page_count)
-        print(list_page)
+        #print(str_page_count)
+        page_count = re.match('共(\d{1,2})件商品', str_page_count).groups()
+        if int(page_count[0]) != 0:
+            print(page_count) #打印分页数
+            if int(page_count[0]) > 20:
+                pass
+                #self.reload_page(response, cate_id)
+            else:
+                self.index_page(response, cate_id)
+
+    # 点击每个商品
+    def index_page(self, response, cate_id):
+        print(cate_id)
+        #print(response.doc('.drug_item').items())
+        for bprder in response.doc('.drug_item').items():
+            print(bprder)
+            self.crawl(bprder('.drug_item_img a').attr('href'),
+                       callback=self.detail_page,
+                       save={'cate_id': cate_id, 'type': 3}
+                       )
+
+    # 点击进入详情
+    @config(priority=2)
+    def detail_page(self, response):
+        cate_id = response.save['cate_id']
+        print(cate_id)
+        good_no = response.doc('#gids').val()
+        name = response.doc('#name').val()
+        price = response.doc('#price').val()
+        set_num = response.doc('#newKuc').text()
+        detail = response.doc('.list_common instructions').html()
+        notice = response.doc('.list_common afterSaleService').html()
+        pic = ''
+        dect = {
+            'name':name, 'cate_id':cate_id, 'good_no':good_no,
+            'price':price,'set_num':set_num, 'pic':pic, 'detail':detail,
+            'notice':notice,
+        }
+        self.goods.append(dect)
+
 
     def write_log(self, str):
         try:
